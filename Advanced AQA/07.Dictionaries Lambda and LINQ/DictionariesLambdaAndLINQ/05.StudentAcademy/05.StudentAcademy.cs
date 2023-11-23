75fa1fe5bfa20150141293746b216057745071ac
Dobromir Damyanov <milchevd@gmail.com>
2023-11-24 00:26:12 +0200

    update

---
+++ Advanced AQA/07.Dictionaries Lambda and LINQ/DictionariesLambdaAndLINQ/05.StudentAcademy/05.StudentAcademy.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, List<double>> students = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) 
+{
+    string student = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(student)) 
+    {
+        students.Add(student, new List<double>());
+    }
+
+    students[student].Add(grade);
+}
+
+var excellent = students.Where(kvp => kvp.Value.Average() >= 4.50);
+
+ foreach (var currentStudent in excellent)
+{
+    Console.WriteLine($"{currentStudent.Key} -> {currentStudent.Value.Average():F2}");
+}
